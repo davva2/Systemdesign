21ec2dd50fb89a88658ff481255c0332432ebb2a
David <davidsmeds@gmail.com>
2017-03-02 15:55:53 +0100

    Version 1.0

--- public/js/diner.js
+++ public/js/diner.js
@@ -10,7 +10,9 @@ function getRandomInt(min, max) {
 function getOrderNumber() {
   // It's probably not a good idea to generate a random order number, client-side. 
   // A better idea would be to let the server decide.
-  return "#" + getRandomInt(1, 100);
+  var e = document.getElementById("bord");
+  var bord = e.options[e.selectedIndex].text;
+  return bord + " #" + getRandomInt(1, 100);
 }
 
 new Vue({
@@ -19,11 +21,14 @@ new Vue({
   methods: {
     placeOrder: function() {
       // create an array of checked items to order
+      var e = document.getElementById("bord");
+      var bord = e.options[e.selectedIndex].text;
     var orderItems = [].filter.call(document.getElementsByName('itemName'), function(i) {
       return i;
     }).map(function(i) {
       return document.getElementById(i.value + "amount").textContent + ' st ' + i.value;
     });
+
       if (orderItems.length != 0) socket.emit('order', {orderId: getOrderNumber(), orderItems: orderItems});
       document.getElementById('rows').removeChild(document.getElementById('bestallningar'));
   }
--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -6,8 +6,21 @@ new Vue({
   mixins: [sharedVueStuff], // include stuff that goes to both diner and kitchen
   methods: {
     markDone: function(orderid) {
+      var button = document.getElementById(orderid); //Hitta knappen som man tryckt på
+
+            //Kolla färg/status på knappen. Ifall röd- gör gul.
+      if (document.getElementById(orderid).className == "pending") {
+      button.setAttribute("style", "background-color: yellow");
+      button.setAttribute('class', 'ongoing');
+            //Ifall gul gör grön.
+      } else if (document.getElementById(orderid).className == "ongoing") {
+      button.setAttribute("style", "background-color: green");
+      button.setAttribute('class', 'ready');
+      } else {
+      //Ifall grön gör klar.
       this.orders[orderid].done = true;
       socket.emit("orderDone", orderid);
+      }
     }
   }
 });
